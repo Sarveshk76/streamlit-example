d28ebeaff11b387593a72fd73907b0f6939e0249
Sarveshk76 <kulkarnisarvesh1@gmail.com>
2023-07-23 14:38:32 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,31 +6,13 @@ import streamlit as st
 
 st.title('Streamlit + Sqlite3')
 
-c = st.experimental_connection("sqlite_db", type="sql")
-conn = c.cursor()
+conn = st.experimental_connection("sqlite_db", type="sql")
+
 if conn is not None:
     st.write('Connected!')
 
     st.write('Creating table...')
     conn.query('CREATE TABLE IF NOT EXISTS data (x, y)')
-    st.write('Done!')
-
-    st.write('Inserting data...')
-    conn.query('INSERT INTO data VALUES (?, ?)', (1, 1))
-    conn.query('INSERT INTO data VALUES (?, ?)', (2, 4))
-    conn.query('INSERT INTO data VALUES (?, ?)', (3, 9))
-    st.write('Done!')
-
-    st.write('Querying...')
-    data = conn.query('SELECT * FROM data').fetchall()
-    st.write('Done!')
-
-    st.write('Data:')
-    st.write(data)
-
-    st.write('Plotting...')
-    df = pd.DataFrame(data, columns=['x', 'y'])
-    st.write(alt.Chart(df).mark_line().encode(x='x', y='y'))
     st.write('Done!')
 
     st.write('Closing connection...')
