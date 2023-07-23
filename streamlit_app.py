dc3350bb05b31bdca3528806c7ad38fd658d2b51
Sarveshk76 <kulkarnisarvesh1@gmail.com>
2023-07-23 18:32:12 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 from datetime import timedelta
 import pandas as pd
 import streamlit as st
+from sqlalchemy.sql import text
 
 st.title('Streamlit + Sqlite3')
 
@@ -10,12 +11,12 @@ conn = st.experimental_connection("sqlite_db", type="sql")
 
 with conn.session as s:
     st.markdown(f"Note that `s` is a `{type(s)}`")
-    s.execute('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);')
-    s.execute('DELETE FROM pet_owners;')
+    s.execute(text('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);'))
+    s.execute(text('DELETE FROM pet_owners;'))
     pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
     for k in pet_owners:
         s.execute(
-            'INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);',
+            text('INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);'),
             params=dict(owner=k, pet=pet_owners[k])
         )
     s.commit()
