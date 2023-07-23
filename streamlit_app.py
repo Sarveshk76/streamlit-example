59ab9ef75d012c633190a85c9b11a52a9467d44f
Sarveshk76 <kulkarnisarvesh1@gmail.com>
2023-07-23 13:22:45 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,8 @@ import streamlit as st
 
 st.title('Streamlit + Sqlite3')
 
-conn = st.experimental_connection(st.secrets["DB_CONN"])
+conn = st.experimental_connection("DB_CONN")
 
-# print(conn)
 
 if conn is not None:
     st.write('Connected!')
@@ -39,23 +38,3 @@ if conn is not None:
     conn.close()
     st.write('Done!')
 
-# with st.echo(code_location='below'):
-#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-#     Point = namedtuple('Point', 'x y')
-#     data = []
-
-#     points_per_turn = total_points / num_turns
-
-#     for curr_point_num in range(total_points):
-#         curr_turn, i = divmod(curr_point_num, points_per_turn)
-#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#         radius = curr_point_num / total_points
-#         x = radius * math.cos(angle)
-#         y = radius * math.sin(angle)
-#         data.append(Point(x, y))
-
-#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#                     .mark_circle(color='#0068c9', opacity=0.5)
-#                     .encode(x='x:Q', y='y:Q'))
