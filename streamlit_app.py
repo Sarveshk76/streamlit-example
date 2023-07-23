16b51b389a1062d9225534eb3db9650977bed641
Sarveshk76 <kulkarnisarvesh1@gmail.com>
2023-07-23 18:29:19 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,24 @@
 from collections import namedtuple
 import altair as alt
-import math
+from datetime import timedelta
 import pandas as pd
 import streamlit as st
 
 st.title('Streamlit + Sqlite3')
 
 conn = st.experimental_connection("sqlite_db", type="sql")
-conn
+
+with conn.session as s:
+    st.markdown(f"Note that `s` is a `{type(s)}`")
+    s.execute('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);')
+    s.execute('DELETE FROM pet_owners;')
+    pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
+    for k in pet_owners:
+        s.execute(
+            'INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);',
+            params=dict(owner=k, pet=pet_owners[k])
+        )
+    s.commit()
+
+pet_owners = conn.query('select * from pet_owners', ttl=timedelta(minutes=10))
+st.dataframe(pet_owners)
